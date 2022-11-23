c9946a92466dff3c52fed15d42cc9033486a9faa
sasaki <1245752030@qq.com>
2022-11-24 00:49:59 +0800

    刘千龙-第一次提交

---
+++ IParams.java
@@ -0,0 +1,16 @@
+package gitskill;
+
+
+import java.util.ArrayList;
+
+public interface IParams {
+
+    public int getBig();
+
+    public int getMedium();
+
+    public int getSmall();
+    public void set(int big,int medium,int small);
+    public ArrayList<Integer> getPlanParking();
+    public void add(int i);
+}
